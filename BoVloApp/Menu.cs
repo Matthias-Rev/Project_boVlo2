506b32c6e8662bc486d1d4fcfc04cac1a07307e0
Nolf195022 <195022@ecam.be>
2022-02-21 14:17:13 +0100

    CLose

--- BoVloApp/Menu.cs
+++ BoVloApp/Menu.cs
@@ -16,18 +16,14 @@ namespace BoVloApp
         {
             InitializeComponent();
             var myBtns = GlobalVar.GetAllButtons(this);
+            int button_size_x = 400;
+            int button_size_y = 100;
             foreach (var btn in myBtns)
             {
                 btn.FlatStyle = FlatStyle.System;
+                btn.Size = new Size(button_size_x, button_size_y);
             }
-            int button_size_x = 400;
-            int button_size_y = 100;
-            List<Button> buttons = new List<Button>;
-            buttons.Add(COMMANDES);
-            buttons.Add(CALENDRIER);
-            buttons.Add(STOCK);
-            buttons.Add(CATALOGUE);
-            COMMANDES.Size = new Size(button_size_x, button_size_y);
+
             COMMANDES.Location = GlobalVar.SetLocation(this, -500,-100, COMMANDES);
         }
         private void COMMANDES_Click(object sender, EventArgs e)
