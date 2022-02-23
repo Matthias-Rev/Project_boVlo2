6e0db4f9916d6fadb763177bf0381a3ae28e6e8a
Matthias-Rev <60757246+Matthias-Rev@users.noreply.github.com>
2022-02-23 17:14:01 +0100

    Done V

--- BoVloApp/Final_Catalogue.cs
+++ BoVloApp/Final_Catalogue.cs
@@ -10,11 +10,31 @@ using System.Windows.Forms;
 
 namespace BoVloApp
 {
-    public partial class Final_Catalogue : Form
+    public partial class Final_Catalogue : BaseForm
     {
         public Final_Catalogue()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // go to the next windows
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            // addd to the database
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            // addd to the database
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            // addd to the database
         }
     }
 }
