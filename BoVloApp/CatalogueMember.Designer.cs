dbf42c02658a9972f8834302e7bd6c5edfc8c18f
HungryEdgar <edgarantunes533@gmail.com>
2022-02-27 21:34:18 +0100

    ENlever les handlers

--- BoVloApp/CatalogueMember.Designer.cs
+++ BoVloApp/CatalogueMember.Designer.cs
@@ -153,7 +153,7 @@
             this.Controls.Add(this.label2);
             this.Name = "CatalogueMember";
             this.Text = "CatalogueMember";
-            this.Load += new System.EventHandler(this.CatalogueMember_Load);
+
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
