258728a7f2df8d58dfaccc24c4a1c895508a70b1
Matthias-Rev <60757246+Matthias-Rev@users.noreply.github.com>
2022-02-27 16:56:39 +0100

    FinalCatalgogue avec transistion

--- BoVloApp/Final_Catalogue.cs
+++ BoVloApp/Final_Catalogue.cs
@@ -17,9 +17,44 @@ namespace BoVloApp
             InitializeComponent();
         }
 
+        public void Clearform()
+        {
+            foreach (Control c in this.Controls)
+            {
+                if (c is Form)
+                {
+                    this.Controls.Remove(c);
+                    (c as Form).Close();
+                }
+            }
+            label1.Visible = true;
+            textBox1.Visible = true;
+            label2.Visible = true;
+            textBox2.Visible = true;
+            label3.Visible = true;
+            textBox3.Visible = true;
+            button1.Visible = true;
+        }
+        public void Loadform(object Form)
+        {
+            Clearform();
+            label1.Visible = false;
+            textBox1.Visible = false;
+            label2.Visible = false;
+            textBox2.Visible = false;
+            label3.Visible = false;
+            textBox3.Visible = false;
+            button1.Visible = false;
+            Form form_to_load = Form as Form;
+            form_to_load.TopLevel = false;
+            form_to_load.Dock = DockStyle.Fill;
+            this.Controls.Add(form_to_load);
+            this.Tag = form_to_load;
+            form_to_load.Show();
+        }
         private void button1_Click(object sender, EventArgs e)
         {
-            // go to the next windows
+            Loadform(new Main());
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
