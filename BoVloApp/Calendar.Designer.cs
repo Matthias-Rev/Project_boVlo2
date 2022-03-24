aa7e424ebabe617a497545c585cd1db82ed086e0
Matthias-Rev <60757246+Matthias-Rev@users.noreply.github.com>
2022-03-24 08:37:09 +0100

    Calendar

---
+++ BoVloApp/Calendar.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace BoVloApp
+{
+    partial class Calendar
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(24, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(280, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Calendrier de Production";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(34, 114);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1422, 62);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // Calendar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1685, 784);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.label1);
+            this.Name = "Calendar";
+            this.Text = "Calendar";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+    }
+}
---
+++ BoVloApp/Calendar.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BoVloApp
+{
+    public partial class Calendar : Form
+    {
+        public Calendar()
+        {
+            InitializeComponent();
+
+            string request = String.Format(
+            "SELECT * " +
+            "FROM calendar " +
+            "WHERE SessionKey='{0}'"
+    ,       GlobalVar.ReadXML().key);
+            DataTable calendar = GlobalVar.ReadSQL(request);
+            string[] titels = { "Command_id", "Start date", "End date"};
+            DataTable data = new();
+            foreach (string titel in titels)
+            {
+                data.Columns.Add(titel);
+            }
+            foreach (DataRow row in calendar.Rows)
+            {
+                DataRow datarow = data.NewRow();
+                datarow["Command_id"] = row["idOrder"].ToString();
+                datarow["Start date"] = row["start"].ToString();
+                datarow["End date"] = row["end"].ToString();
+                //datarow["Price"] = Int32.Parse(GlobalVar.types.Select(String.Format("idBike = '{0}'", row["idBike"].ToString()))[0]["Price"].ToString());
+                //datarow["Size"] = row["Size"].ToString();
+                //datarow["Color"] = GlobalVar.colors.Select(String.Format("idColor = '{0}'", row["idColor"].ToString()))[0]["Name"].ToString();
+                data.Rows.Add(datarow);
+                //LabelPrixTotal.Text = (Int32.Parse(row["Quantity"].ToString()) * Int32.Parse(datarow["Price"].ToString()) + Int32.Parse(LabelPrixTotal.Text)).ToString();
+            }
+            GlobalVar.DisplayTableByData(titels, data, tableLayoutPanel1);
+            GlobalVar.SetLocation(this, -100, 0, tableLayoutPanel1);
+        }
+    }
+}
