67cbb671b27567cae0963e088dbf14281e720783
Matthias-Rev <60757246+Matthias-Rev@users.noreply.github.com>
2022-05-09 18:37:15 +0200

    Calendar update

--- BoVloApp/Calendar.Designer.cs
+++ BoVloApp/Calendar.Designer.cs
@@ -29,16 +29,11 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.Panel_calendar = new System.Windows.Forms.Panel();
-            this.checkBox4 = new System.Windows.Forms.CheckBox();
-            this.checkBox3 = new System.Windows.Forms.CheckBox();
-            this.checkBox2 = new System.Windows.Forms.CheckBox();
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
+            this.Calendar_grid = new System.Windows.Forms.DataGridView();
             this.label2 = new System.Windows.Forms.Label();
             this.Panel_calendar.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Calendar_grid)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -47,110 +42,38 @@
             this.label1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.label1.Location = new System.Drawing.Point(24, 23);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(280, 32);
+            this.label1.Size = new System.Drawing.Size(231, 32);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Calendrier de Production";
-            // 
-            // tableLayoutPanel1
-            // 
-            this.tableLayoutPanel1.BackColor = System.Drawing.SystemColors.ScrollBar;
-            this.tableLayoutPanel1.ColumnCount = 3;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(23, 166);
-            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 4;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(1422, 109);
-            this.tableLayoutPanel1.TabIndex = 1;
+            this.label1.Text = "Calendar Production";
             // 
             // Panel_calendar
             // 
-            this.Panel_calendar.Controls.Add(this.checkBox4);
-            this.Panel_calendar.Controls.Add(this.checkBox3);
-            this.Panel_calendar.Controls.Add(this.checkBox2);
-            this.Panel_calendar.Controls.Add(this.checkBox1);
-            this.Panel_calendar.Controls.Add(this.tableLayoutPanel1);
-            this.Panel_calendar.Controls.Add(this.label4);
-            this.Panel_calendar.Controls.Add(this.label3);
             this.Panel_calendar.Controls.Add(this.label2);
+            this.Panel_calendar.Controls.Add(this.Calendar_grid);
             this.Panel_calendar.Location = new System.Drawing.Point(1, -1);
             this.Panel_calendar.Name = "Panel_calendar";
             this.Panel_calendar.Size = new System.Drawing.Size(1688, 788);
             this.Panel_calendar.TabIndex = 2;
             // 
-            // checkBox4
+            // Calendar_grid
             // 
-            this.checkBox4.AutoSize = true;
-            this.checkBox4.BackColor = System.Drawing.SystemColors.Control;
-            this.checkBox4.Location = new System.Drawing.Point(1482, 261);
-            this.checkBox4.Name = "checkBox4";
-            this.checkBox4.Size = new System.Drawing.Size(15, 14);
-            this.checkBox4.TabIndex = 6;
-            this.checkBox4.UseVisualStyleBackColor = false;
-            // 
-            // checkBox3
-            // 
-            this.checkBox3.AutoSize = true;
-            this.checkBox3.BackColor = System.Drawing.SystemColors.Control;
-            this.checkBox3.Location = new System.Drawing.Point(1482, 233);
-            this.checkBox3.Name = "checkBox3";
-            this.checkBox3.Size = new System.Drawing.Size(15, 14);
-            this.checkBox3.TabIndex = 5;
-            this.checkBox3.UseVisualStyleBackColor = false;
-            // 
-            // checkBox2
-            // 
-            this.checkBox2.AutoSize = true;
-            this.checkBox2.BackColor = System.Drawing.SystemColors.Control;
-            this.checkBox2.Location = new System.Drawing.Point(1482, 204);
-            this.checkBox2.Name = "checkBox2";
-            this.checkBox2.Size = new System.Drawing.Size(15, 14);
-            this.checkBox2.TabIndex = 4;
-            this.checkBox2.UseVisualStyleBackColor = false;
-            // 
-            // checkBox1
-            // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(1482, 175);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(15, 14);
-            this.checkBox1.TabIndex = 3;
-            this.checkBox1.UseVisualStyleBackColor = true;
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label4.Location = new System.Drawing.Point(1170, 128);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(83, 20);
-            this.label4.TabIndex = 2;
-            this.label4.Text = "Date de fin";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label3.Location = new System.Drawing.Point(682, 128);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(105, 20);
-            this.label3.TabIndex = 1;
-            this.label3.Text = "Date de début";
+            this.Calendar_grid.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.Calendar_grid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Calendar_grid.Location = new System.Drawing.Point(237, 120);
+            this.Calendar_grid.Name = "Calendar_grid";
+            this.Calendar_grid.RowTemplate.Height = 25;
+            this.Calendar_grid.Size = new System.Drawing.Size(912, 554);
+            this.Calendar_grid.TabIndex = 3;
+            this.Calendar_grid.CellEndEdit += new System.Windows.Forms.DataGridViewCellEventHandler(this.Calendar_grid_CellEndEdit);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label2.Location = new System.Drawing.Point(171, 128);
+            this.label2.Location = new System.Drawing.Point(1009, 52);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(132, 20);
-            this.label2.TabIndex = 0;
-            this.label2.Text = "Nom de la Société";
+            this.label2.Size = new System.Drawing.Size(38, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "label2";
             // 
             // Calendar
             // 
@@ -164,6 +87,7 @@
             this.Text = "Calendar";
             this.Panel_calendar.ResumeLayout(false);
             this.Panel_calendar.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Calendar_grid)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -172,14 +96,8 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
         private System.Windows.Forms.Panel Panel_calendar;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView Calendar_grid;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.CheckBox checkBox4;
-        private System.Windows.Forms.CheckBox checkBox3;
-        private System.Windows.Forms.CheckBox checkBox2;
-        private System.Windows.Forms.CheckBox checkBox1;
     }
 }
--- BoVloApp/Calendar.cs
+++ BoVloApp/Calendar.cs
@@ -12,31 +12,83 @@ namespace BoVloApp
 {
     public partial class Calendar : Form
     {
-        public Calendar()
+            public Calendar()
         {
             InitializeComponent();
+            DisplayOrder();
+        }
 
-            string request = String.Format(
-            "SELECT * " +
-            "FROM calendar "
-    ,       GlobalVar.ReadXML().key);
-            DataTable calendar = GlobalVar.ReadSQL(request);
-            string[] titels = { "Command_id", "Start date", "End date"};
-            DataTable data = new();
-            foreach (string titel in titels)
+        private void DisplayOrder()
+        {
+            DataTable order = GetMore();
+            //DataTable order = GetCalendar();
+            Calendar_grid.DataSource = order;
+            Calendar_grid.Columns["Start"].DefaultCellStyle.Format = "yyyy/MM/dd";
+        }
+
+            
+        private DataTable GetCalendar()
+        {
+            string request = "SELECT Calendar " +
+                "FROM Calendar ";
+            DataTable dates = GlobalVar.ReadSQL(request);
+
+            return dates;
+
+        }
+        private DataTable GetMore()
+        {
+            string request = "SELECT Customer.Name, Calendar.idOrder,Calendar.Start, Calendar.End " +
+                "FROM Orders, Customer, Calendar " +
+                "WHERE Calendar.idOrder = Orders.idOrder " +
+                "AND Orders.Customer_id = Customer.Customer_id";
+
+            DataTable order = GlobalVar.ReadSQL(request);
+
+
+            return order;
+
+        }
+        private DataTable Update_date()
+        {
+            string request = "SELECT Customer.Name ,Calendar.idOrder, Calendar.Start, Calendar.End " +
+                "FROM Orders, Customer, Calendar " +
+                "WHERE Calendar.idOrder = Orders.idOrder " +
+                "AND Orders.Customer_id = Customer.Customer_id";
+            DataTable order = GlobalVar.ReadSQL(request);
+            return order;
+        }
+
+        private void Calendar_grid_CellEndEdit(object sender, DataGridViewCellEventArgs e)
+        {
+            DataGridView update = sender as DataGridView;
+            if (update.Rows[e.RowIndex].Cells[e.ColumnIndex].Value != null)
             {
-                data.Columns.Add(titel);
+
+                update.CurrentRow.Selected = true;
+                var val = update.Rows[e.RowIndex].Cells[e.ColumnIndex].FormattedValue.ToString();
+                var name_col = update.Columns[e.ColumnIndex].Name;
+                var name_column = update.Rows[e.RowIndex].Cells["idOrder"].FormattedValue.ToString();
+
+                //------------------Marche pas-----------------------
+                //string request = "SELECT * " +
+                //    "FROM OrderDetails, Orders, Customer, Basket, Color " +
+                //    "WHERE OrderDetails.idOrder = Orders.idOrder " +
+                //    "AND OrderDetails.idArticle = Basket.idArticle " +
+                //    "AND OrderDetails.idBike = Bike.idBike " +
+                //    "AND OrdeDetails.idColor = Color.idColor ";
+                //----------------------------------------------------
+
+                string request = "UPDATE Calendar SET " + name_col + " = CAST("+ val +" AS DATETIME) WHERE idOrder = " + name_column;
+                DataTable orderDetail = GlobalVar.ReadSQL(request);
+
+                update.DataSource = orderDetail;
+                GetMore();
+
+                //-------------------------This label is used for debugging purposes
+                //label2.Text = val.ToString();
+
             }
-            foreach (DataRow row in calendar.Rows)
-            {
-                DataRow datarow = data.NewRow();
-                datarow["Command_id"] = row["idOrder"].ToString();
-                datarow["Start date"] = row["start"].ToString();
-                datarow["End date"] = row["end"].ToString();
-                data.Rows.Add(datarow);
-            }
-            //GlobalVar.DisplayTableByData(titels, data, tableLayoutPanel1);
-            GlobalVar.SetLocation(this, -100, 0, tableLayoutPanel1);
         }
     }
 }
