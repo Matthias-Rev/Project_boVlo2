6ebbab0e0e4323be819f032319353d7ca837ac59
JordanHermans <78797946+JordanHermans@users.noreply.github.com>
2022-06-06 22:07:59 +0200

    Add files via upload

---
+++ Database/Mysqlconn.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace BoVloApp
+{
+    public class Mysqlconn
+    {
+        static public MySqlConnection connect()
+        {
+            MySqlConnection connection;
+            string connetionString = "server = ***; port = ***; database = Bovlo; uid = ***; pwd = ***;";
+            connection = new MySqlConnection(connetionString);
+            try
+            {
+                connection.Open();
+                return connection;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+                return connection;
+            }
+        }
+        static public void disconnect(MySqlConnection connection)
+        {
+            connection.Close();
+        }
+    }
+}
+
+
+
+
+
